6fb43c25fd3f9b42cf7753fea937c9f24a974bc4
yahya354 <yahyafirdaus354@gmail.com>
2018-05-10 19:51:54 +0700

    Update

--- application/views/ad_login.php
+++ application/views/ad_login.php
@@ -1,4 +1,4 @@
-<form class="container p-5" method="post" action="<?php echo('http://localhost:8080/TP3/welcome/show'); ?>">
+<form class="container p-5" method="post" action="<?php echo('http://localhost:8080/Gundangku-CI/welcome/show'); ?>">
 	<div class="row justify-content-center">
 		<div class="col-2 text-center">
 			<input type="submit" name="submit1" class="btn btn-info" value="Daftar Barang">
