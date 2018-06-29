8f993463a17a79c3a130b1789972af2885e21556
Muhammad Nabillah <abel.rischa@gmail.com>
2018-06-29 14:48:58 +0700

    globals

--- application/libraries/Globals.php
+++ application/libraries/Globals.php
@@ -1,19 +1,21 @@
 <?php
+
 class Globals
 {
-    function __construct() {
-        // $this->api = 'http://7fd6254b.ngrok.io/api/Parts';
+    function __construct()
+    {
         $this->api = 'http://localhost:5000/api/Parts';
+//        $this->api = 'http://77289763.ngrok.io/api/Parts';
         $this->options = array(
             CURLOPT_RETURNTRANSFER => true,   // return web page
-            CURLOPT_HEADER         => false,  // don't return headers
+            CURLOPT_HEADER => false,  // don't return headers
             CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-            CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-            CURLOPT_ENCODING       => "",     // handle compressed
-            CURLOPT_USERAGENT      => "test", // name of client
-            CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+            CURLOPT_MAXREDIRS => 10,     // stop after 10 redirects
+            CURLOPT_ENCODING => "",     // handle compressed
+            CURLOPT_USERAGENT => "test", // name of client
+            CURLOPT_AUTOREFERER => true,   // set referrer on redirect
             CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-            CURLOPT_TIMEOUT        => 120,    // time-out on response
+            CURLOPT_TIMEOUT => 120,    // time-out on response
         );
     }
 }
